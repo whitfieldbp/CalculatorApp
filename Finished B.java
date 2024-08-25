e7b7f360ab48abbf3869be5dfd43afc582b5bd7a
Ben Whitfield <bpw0030@auburn.edu>
2024-08-25 13:11:23 -0500

    Added C.java

---
+++ Finished B.java
@@ -0,0 +1,24 @@
+[33mcommit 6793b664c711c3038980cd4efdbda9e093b0db3b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mdevelopment[m[33m)[m
+Author: Ben Whitfield <bpw0030@auburn.edu>
+Date:   Sun Aug 25 13:04:17 2024 -0500
+
+    Finished B.java
+
+[33mcommit 76ef618b3311a6be87b78a4d543063e73d539587[m
+Author: Ben Whitfield <bpw0030@auburn.edu>
+Date:   Sun Aug 25 12:54:47 2024 -0500
+
+    Now saving the world
+
+[33mcommit d930cdd7c6ec63f4b304e0900fc7e37d360bdb0e[m[33m ([m[1;33mtag: [m[1;33mv1.0[m[33m, [m[1;32mmain[m[33m)[m
+Author: Ben Whitfield <bpw0030@auburn.edu>
+Date:   Fri Aug 23 21:08:00 2024 -0500
+
+    Added A.java and B.java, modified Calc.java with
+    goodbye message.
+
+[33mcommit e86d2023b70c018edb62f99da48e9bb686b7e48c[m
+Author: Ben Whitfield <bpw0030@auburn.edu>
+Date:   Fri Aug 23 21:00:49 2024 -0500
+
+    Added Calc.java
