76ef618b3311a6be87b78a4d543063e73d539587
Ben Whitfield <bpw0030@auburn.edu>
2024-08-25 12:54:47 -0500

    Now saving the world

--- B.java
+++ B.java
@@ -1,5 +1,7 @@
 public class B {
     public static void main(String args[]) {
-
+        public void saveTheWorld() {
+            
+        }
     }
 }
--- Calc.java
+++ Calc.java
@@ -1,5 +1,5 @@
 public class Calc {
     public static void main(String args[]) {
-        System.out.println("Goodbye, Cruel World!");
+        System.out.println("Hello, World!");
     }
 }
